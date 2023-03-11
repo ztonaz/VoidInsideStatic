9325e88f1d83919eb44017071bacea4feb326a2c
ztonaz <49240883+ztonaz@users.noreply.github.com>
2023-03-11 14:48:03 +0100

    Update TriviaProjectApplicationTests.java

--- TriviaProject/src/test/java/com/miTrabajo/mt/TriviaProjectApplicationTests.java
+++ TriviaProject/src/test/java/com/miTrabajo/mt/TriviaProjectApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TriviaProjectApplicationTests {
+class VoidInsideStaticTests {
 
 	@Test
 	void contextLoads() {
